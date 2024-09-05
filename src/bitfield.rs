51c34f0f0ac194611d1e364c1afe3e0969fb2743
Jordan Bourdeau <jordan.bourdeau@uvm.edu>
2024-09-05 14:47:27 -0400

    Refactor to be byte-aligned. Could use more rigorous testing for boundary conditions.

--- src/bitfield.rs
+++ src/bitfield.rs
@@ -2,32 +2,30 @@ use paste::paste;
 
 use super::errors::BitfieldError;
 
+const BITS_IN_BYTE: usize = 8;
+const BITS_IN_WORD: usize = core::mem::size_of::<usize>() * BITS_IN_BYTE;
+
 macro_rules! ceiling {
     ($value:expr) => {
-        if value > (value as usize as f32) {
-            (value + 1.0) as usize
+        if $value > ($value as usize as f32) {
+            ($value + 1.0) as usize
         } else {
-            value as usize
+            $value as usize
         }
     };
 }
 
-macro_rules! words_required {
+/// Figure out how many bytes are requires to represent a value
+macro_rules! bytes_required {
     ($($bits:expr),*) => {
-        {
-            const num_bits: usize = 0 $(+$bits)* ;
-            const word_size: usize = core::mem::size_of::<usize>() * 8;
-            const value: f32 = (num_bits as f32) / (word_size as f32);
-            ceiling!(value)
-        }
+        ceiling!((0 $(+$bits)*) as f32 / BITS_IN_BYTE as f32)
     };
 }
 
 #[macro_export]
 macro_rules! bitfield {
     ($name:ident { $(($field:ident, $bits:expr)),* $(,)? }) => {
-
-
+        /// Figure out how many bits to skip before we get to the target
         macro_rules! shift {
             ($target:ident) => {
                 {
@@ -46,83 +44,103 @@ macro_rules! bitfield {
 
 
         #[derive(Debug)]
+        #[repr(packed)]
         struct $name {
-            words: [usize; words_required!($($bits),*)],
+            bytes: [u8; bytes_required!($($bits),*)],
         };
 
-        impl $name {
+         impl $name {
             pub fn new() -> $name {
                 $name {
-                    words: [0; words_required!($($bits),*)],
+                    bytes: [0; bytes_required!($($bits),*)],
                 }
             }
 
             #[inline]
-            fn word_index(shift: usize) -> usize {
-                shift / Self::word_bits()
+            fn byte_index(shift: usize) -> usize {
+                shift / BITS_IN_BYTE
             }
 
             #[inline]
-            fn word_bits() -> usize {
-                core::mem::size_of::<usize>() * 8
+            pub fn size_bytes(&self) -> usize {
+                self.bytes.len()
             }
         }
 
         paste::paste! {
             $(
-                if $bits > core::mem::size_of::<usize>() * 8 {
-                    panic!("Items in bitfield > 64 bits not supported");
+                if $bits > BITS_IN_WORD {
+                    panic!("Items in bitfield > {} bits not supported", BITS_IN_WORD);
                 }
                 impl $name {
                     pub fn [<get_ $field>](&self) -> usize {
-                        let shift = shift!($field);
-                        let word_index = Self::word_index(shift);
+                        let start_bit = shift!($field);
+                        let end_bit = start_bit + $bits;
+                        let start_offset = start_bit % BITS_IN_BYTE;
+                        let end_offset = BITS_IN_BYTE - end_bit % BITS_IN_BYTE;
+                        let mut byte_index = Self::byte_index(start_bit);
+                        let mut current_bit = start_bit + start_offset;
+                        let mut value = 0;
 
-                        // Beginning bits of the word
-                        let mut result = self.words[word_index] >> (shift % Self::word_bits());
-
-                        let bits_from_boundary = (shift % Self::word_bits() + $bits) as isize - Self::word_bits() as isize;
-
-                        if bits_from_boundary > 0 {
-                            // Crossed a boundary, get remaining bits
-                            let mask = (self.words[word_index + 1] & ((1 << bits_from_boundary) - 1));
-                            result |= (mask << ($bits - bits_from_boundary));
+                        while current_bit <= end_bit {
+                            value <<= BITS_IN_BYTE;
+                            value |= self.bytes[byte_index] as usize;
+                            current_bit += BITS_IN_BYTE;
+                            byte_index += 1;
                         }
+                        value >>= end_offset;
 
-                        result & (std::usize::MAX >> (Self::word_bits() - $bits))
+                        value
                     }
-
-                    pub fn [<set_ $field>](&mut self, value: usize) -> Result<bool, $crate::errors::BitfieldError> {
-                        if value > 2_usize.pow($bits) - 1 {
-                            return Err($crate::errors::BitfieldError::ValueTooLarge{ value, num_bits: $bits });
+                    
+                    pub fn [<set_ $field>](&mut self, mut value: usize) -> Result<bool, $crate::errors::BitfieldError> {
+                        if value > (2_usize.pow($bits) - 1) {
+                            return Err($crate::errors::BitfieldError::ValueTooLarge { value, num_bits: $bits });
                         }
-                        let shift = shift!($field);
-                        let word_index = Self::word_index(shift);
-                        let local_shift = shift % Self::word_bits();
-                        let mask = (!value << local_shift) | ((1 << local_shift) - 1);
 
-                        self.words[word_index] &= mask;
-                        self.words[word_index] |= (value << local_shift);
+                        let start_bit = shift!($field);
+                        let end_bit = start_bit + $bits;
+                        let start_offset = start_bit % BITS_IN_BYTE;
+                        let end_offset = BITS_IN_BYTE - end_bit % BITS_IN_BYTE;
+                        let mut byte_index = Self::byte_index(start_bit);
+                        let mut current_bit = start_bit + start_offset;
 
-                        let bits_from_boundary = (shift % Self::word_bits() + $bits) as isize - Self::word_bits() as isize;
+                        // Update mask as we go
+                        // Slow implementation due to using usize for bits
+                        let mut mask = (2_usize.pow($bits as u32) - 1) << start_offset;
+                        while current_bit <= end_bit {
+                            let byte_mask = mask & std::u8::MAX as usize;
+                            let byte_value = (value & byte_mask) >> start_offset;
 
-                        if bits_from_boundary > 0 {
-                            // Crossed a boundary, get remaining bits
-                            let mask = (self.words[word_index + 1] << (bits_from_boundary as usize));
-                            self.words[word_index + 1] &= (mask | (value >> ($bits - bits_from_boundary)));
+                            self.bytes[byte_index] &= !byte_mask as u8;
+                            self.bytes[byte_index] |= byte_value as u8;
+
+                            current_bit += BITS_IN_BYTE;
+                            byte_index += 1;
+                            mask >>= BITS_IN_BYTE;
+                            value >>= BITS_IN_BYTE;
                         }
+                         
 
                         Ok(true)
                     }
+
+       
                 }
             )*
         }
-    };
-}
+    };}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_ceiling() {
+        assert_eq!(ceiling!(65.0 / 8.0), 9);
+        assert_eq!(ceiling!(0.0 / 8.0), 0);
+        assert_eq!(ceiling!(8.0 / 8.0), 1);
+    }
 
     #[test]
     fn test_struct_create() {
@@ -134,7 +152,7 @@ mod tests {
             }
         };
         let t = Test2::new();
-        assert_eq!(1, t.words.len());
+        assert_eq!(8, t.size_bytes());
         bitfield! {
             Test3 {
                 (a, 64),
@@ -142,14 +160,14 @@ mod tests {
             }
         };
         let t = Test3::new();
-        assert_eq!(2, t.words.len());
+        assert_eq!(9, t.size_bytes());
         bitfield! {
             Test4 {
                 (a, 0)
             }
         };
         let t = Test4::new();
-        assert_eq!(0, t.words.len());
+        assert_eq!(0, t.size_bytes());
     }
 
     #[test]
@@ -163,10 +181,12 @@ mod tests {
         };
 
         let mut t = Test::new();
-        t.words[0] = std::u64::MAX as usize;
+        for i in 0..t.size_bytes() {
+            t.bytes[i] = std::u8::MAX ;
+        }
         assert_eq!(t.get_a(), std::u32::MAX as usize);
         assert_eq!(t.get_b(), std::u16::MAX as usize);
-        assert_eq!(t.get_c(), 0x1F);
+        assert_eq!(t.get_c(), (std::u8::MAX >> 3) as usize);
     }
 
     #[test]
@@ -180,18 +200,27 @@ mod tests {
         };
 
         let mut t = Test::new();
-        t.set_a(6).unwrap();
-        assert_eq!(t.words[0], 6);
+        // Overwrite behavior gets rid of extra bits
         t.set_a(7).unwrap();
-        assert_eq!(t.words[0], 7);
+        assert_eq!(t.bytes[0], 7);
+        t.set_a(6).unwrap();
+        println!("{:?}", t);
+        assert_eq!(t.bytes[0], 6);
+
+        // Check writing a value too larger
         let value = 2_usize.pow(5);
         let expected = Err(BitfieldError::ValueTooLarge {
             value: value,
             num_bits: 5,
         });
         assert_eq!(t.set_c(value), expected);
-        assert_eq!(t.words[0], 7);
-        t.set_c(6).unwrap();
-        assert_eq!(t.words[0], (6 << 48) | 7);
+        let expected = [6, 0, 0, 0, 0, 0, 0];
+        assert_eq!(expected, t.bytes);
+
+        // Check maximum value
+        let value = 2_usize.pow(5) - 1;
+        t.set_c(value).unwrap();
+        let expected = [6, 0, 0, 0, 0, 0, value as u8];
+        assert_eq!(expected, t.bytes);
     }
 }
